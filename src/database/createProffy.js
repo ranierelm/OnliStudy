9fd5342f3232bcfa2a8bb32b01746c6dfeb5ca7b
Raniere Lima <raniereprojeto@gmail.com>
2020-08-11 21:34:52 -0300

    Install sqlite and iniciate configured database

---
+++ src/database/createProffy.js
@@ -0,0 +1,23 @@
+module.exports = async function (db, {proffyValue, classValue, classScheduleValue}){
+    // inserir dados na table de teachers 
+    
+    const insertedProffy = await db.run(`
+        INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            ${proffyValue.name},
+            ${proffyValue.avatar},
+            ${proffyValue.whatsapp},
+            ${proffyValue.bio}
+        );
+    `);
+
+    const proffy_id = insertedProffy.lastID
+    
+}
+
+//parando em 52:55
+
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const proffys = [
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "81985851212",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
         cost: "20",
         weekday:[0],
@@ -14,7 +14,7 @@ const proffys = [
         name: "Daniele Evangelista",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "81985851212",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
         cost: "20",
         weekday:[1],
@@ -25,7 +25,7 @@ const proffys = [
         name: "Raniere Lima",
         avatar: "https://avatars3.githubusercontent.com/u/66389279?s=460&u=b3620b60ab1453941ba62947359d1421eda7315f&v=4",
         whatsapp: "81985851212",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        bio: "Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
         cost: "20",
         weekday:[1],
@@ -34,22 +34,66 @@ const proffys = [
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+//funcionalidades
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
 
 function pageLanding (req, res) {
     return res.render("index.html")
 }
 function pageStudy (req, res) {
-    return res.render("study.html", { proffys, title: "Hi from Nunjucks"})
+    const filters = req.query //recebe no backend a requisição de filtragem
+    return res.render("study.html", { proffys, filters, subjects, weekdays})
 }
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+    
+    // se tiver dados 
+    const isNotEmpty = Object.keys(data).length != 0
+    if(isNotEmpty){
+
+        data.subject = getSubject(data.subject)
+        //adicionar data a lista de Proffys
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }    
+    // se não tiver dados, mostrar a página
+    return res.render("give-classes.html", {subjects, weekdays})
 }
 
+//servidor
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
 
-//configurar nunjucks
+//configurar nunjucks(Template Engine)
+const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
     noCache: true,
